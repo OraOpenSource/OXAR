b12f99e73ccf8859bb28e96bace49eabb5a792ed
Martin D'Souza <martin.dsouza@gmail.com>
2015-02-05 11:57:22 -0700

    Removed reference to sudo

--- scripts/ratom.sh
+++ scripts/ratom.sh
@@ -5,5 +5,5 @@
 #This is to make files easier to edit in Atom
 cd $OOS_SOURCE_DIR/tmp
 curl -o /usr/local/bin/rmate https://raw.githubusercontent.com/aurora/rmate/master/rmate
-sudo chmod +x /usr/local/bin/rmate
+chmod +x /usr/local/bin/rmate
 mv /usr/local/bin/rmate /usr/local/bin/ratom
