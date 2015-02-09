e2fafce2fdc94703e29bb9172eb870cad86d4ddc
Martin D'Souza <martin.dsouza@gmail.com>
2015-02-09 12:25:49 -0700

    Increased the amount of swap space by 2 MB. Required for Fedora 21 support.

--- scripts/swap_space.sh
+++ scripts/swap_space.sh
@@ -2,7 +2,7 @@
 
 #*** SWAP SPACE ***
 #http://www.cyberciti.biz/faq/linux-add-a-swap-file-howto/
-dd if=/dev/zero of=/swapfile1 bs=1024 count=2097152
+dd if=/dev/zero of=/swapfile1 bs=1024 count=2099200
 mkswap /swapfile1
 chown root:root /swapfile1
 chmod 0600 /swapfile1
