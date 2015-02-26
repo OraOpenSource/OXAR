2c59295788cb1c78173a102ea262a00ab4aaa9e3
Martin D'Souza <martin.dsouza@gmail.com>
2015-02-26 08:16:52 -0700

    #17 Move config validations to separate file

--- config.sh
+++ config.sh
@@ -115,32 +115,5 @@ OOS_TC_USERNAME=tomcat
 OOS_TC_PWD=oracle
 
 
-
-#*** VALIDATIONS ***
-# (don't modify the CHANGEME here)
-if [ "$OOS_MODULE_ORACLE" = "Y" ]; then
-  if [ "$OOS_ORACLE_FILE_URL" = "CHANGEME" ] || [ "$OOS_ORACLE_FILE_URL" = "" ]; then
-    echo OOS_ORACLE_FILE_URL must be specified
-    exit 1
-  else
-    echo OOS_ORACLE_FILE_URL=$OOS_ORACLE_FILE_URL
-  fi
-fi
-
-if [ "$OOS_MODULE_APEX" = "Y" ]; then
-  if [ "$OOS_APEX_FILE_URL" = "CHANGEME" ] || [ "$OOS_APEX_FILE_URL" = "" ]; then
-    echo OOS_APEX_FILE_URL must be specified
-    exit 1
-  else
-    echo OOS_APEX_FILE_URL=$OOS_APEX_FILE_URL
-  fi
-fi
-
-if [ "$OOS_MODULE_ORDS" = "Y" ]; then
-  if [ "$OOS_ORDS_FILE_URL" = "CHANGEME" ] || [ "$OOS_ORDS_FILE_URL" = "" ]; then
-    echo OOS_ORDS_FILE_URL must be specified
-    exit 1
-  else
-    echo OOS_ORDS_FILE_URL=$OOS_ORDS_FILE_URL
-  fi
-fi
+#Call Validations
+source ./scripts/config_validation.sh
---
+++ scripts/config_validation.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#*** VALIDATIONS ***
+#Moved to separate file for issue #17
+echo; echo Config Validations; echo;
+
+# (don't modify the CHANGEME here)
+if [ "$OOS_MODULE_ORACLE" = "Y" ]; then
+  if [ "$OOS_ORACLE_FILE_URL" = "CHANGEME" ] || [ "$OOS_ORACLE_FILE_URL" = "" ]; then
+    echo OOS_ORACLE_FILE_URL must be specified
+    exit 1
+  else
+    echo OOS_ORACLE_FILE_URL=$OOS_ORACLE_FILE_URL
+  fi
+fi
+
+if [ "$OOS_MODULE_APEX" = "Y" ]; then
+  if [ "$OOS_APEX_FILE_URL" = "CHANGEME" ] || [ "$OOS_APEX_FILE_URL" = "" ]; then
+    echo OOS_APEX_FILE_URL must be specified
+    exit 1
+  else
+    echo OOS_APEX_FILE_URL=$OOS_APEX_FILE_URL
+  fi
+fi
+
+if [ "$OOS_MODULE_ORDS" = "Y" ]; then
+  if [ "$OOS_ORDS_FILE_URL" = "CHANGEME" ] || [ "$OOS_ORDS_FILE_URL" = "" ]; then
+    echo OOS_ORDS_FILE_URL must be specified
+    exit 1
+  else
+    echo OOS_ORDS_FILE_URL=$OOS_ORDS_FILE_URL
+  fi
+fi
