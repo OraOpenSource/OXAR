c2ae5702be1dc8f0de09c3bc1ae40f847c067f9d
Chris Freeman <chris@ircsinc.com>
2015-03-05 15:32:34 -0600

    added validation for OS type

--- scripts/config_validation.sh
+++ scripts/config_validation.sh
@@ -5,6 +5,14 @@
 echo; echo Config Validations; echo;
 
 # (don't modify the CHANGEME here)
+
+if [ "$OOS_OS_TYPE" = "" ]; then
+  echo OOS_OS_TYPE must be specified to RHEL or Debian
+  exit 1
+else
+  echo OOS_OS_TYPE=$OOS_OS_TYPE
+fi
+
 if [ "$OOS_MODULE_ORACLE" = "Y" ]; then
   if [ "$OOS_ORACLE_FILE_URL" = "CHANGEME" ] || [ "$OOS_ORACLE_FILE_URL" = "" ]; then
     echo OOS_ORACLE_FILE_URL must be specified
