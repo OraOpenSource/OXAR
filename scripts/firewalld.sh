0b5d25feb5e45290d003429ea7f03129c4622de3
Martin D'Souza <martin.dsouza@gmail.com>
2015-04-07 17:21:18 -0600

    #53 Additional Firewall config

--- scripts/firewalld.sh
+++ scripts/firewalld.sh
@@ -9,16 +9,37 @@ service firewalld start
 #Can copy other examples from: /usr/lib/firewalld/services/
 cd /etc/firewalld/services
 
-\cp $OOS_SOURCE_DIR/scripts/tomcat.xml .
+cp $OOS_SOURCE_DIR/firewalld/*.xml .
+
+#Replace dynamic ports
+perl -i -p -e "s/OOS_ORACLE_TNS_PORT/$OOS_ORACLE_TNS_PORT/g" oracle.xml
+
 
 systemctl start firewalld
 firewall-cmd --zone=public --add-service=http --permanent
 firewall-cmd --reload
 
-#To add Tomcat just run these scripts
+#To add Tomcat/Oracle just run these scripts
 #service firewalld stop
 #service firewalld start
+
+#Note: add the --permanent for permanent usage
 #firewall-cmd --zone=public --add-service=tomcat
 #firewall-cmd --zone=public --remove-service=tomcat
 
+if [ "$OOS_FIREWALL_TOMCAT_YN" == "Y" ]; then
+  firewall-cmd --zone=public --add-service=tomcat --permanent
+fi
+
+if [ "$OOS_FIREWALL_ORACLE_YN" == "Y" ]; then
+  firewall-cmd --zone=public --add-service=oracle --permanent
+fi
+
+#Reload for any changes in above config
+firewall-cmd --reload
+
 systemctl enable firewalld
+
+
+#List zone info for logs
+firewall-cmd --zone=public --list-all
