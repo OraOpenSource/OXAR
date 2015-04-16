a5afa1d6b27a480264081b1ab076eb9693635505
fuzziebrain <png.adrian@yahoo.com>
2015-04-16 15:13:28 -0700

    Fixes for Vagrant

--- scripts/ords.sh
+++ scripts/ords.sh
@@ -21,14 +21,14 @@ java -jar ords.war configdir /ords/conf
 #ORDS2
 if [ "$OOS_DEPLOY_TYPE" == "VAGRANT" ]; then
   #Replace mnemonics
-  perl -i -p -e "s/OOS_APEX_PUB_USR_PWD/$OOS_APEX_PUB_USR_PWD/g" ../ords/defaults.properties
-  perl -i -p -e "s/OOS_ORACLE_TNS_PORT/$OOS_ORACLE_TNS_PORT/g" ../ords/defaults.properties
+  perl -i -p -e "s/OOS_APEX_PUB_USR_PWD/$OOS_APEX_PUB_USR_PWD/g" $OOS_SOURCE_DIR/ords/defaults.properties
+  perl -i -p -e "s/OOS_ORACLE_TNS_PORT/$OOS_ORACLE_TNS_PORT/g" $OOS_SOURCE_DIR/ords/defaults.properties
 
   # Attempt silent ORDS configuration if provisioned though Vagrant
-  java -jar ords.war set-properties --conf defaults /vagrant/ords/defaults.properties
-  java -jar ords.war set-properties --conf apex /vagrant/ords/apex.properties
-  java -jar ords.war set-properties --conf apex_al /vagrant/ords/apex_al.properties
-  java -jar ords.war set-properties --conf apex_rt /vagrant/ords/apex_rt.properties
+  java -jar ords.war set-properties --conf defaults $OOS_SOURCE_DIR/ords/defaults.properties
+  java -jar ords.war set-properties --conf apex $OOS_SOURCE_DIR/ords/apex.properties
+  java -jar ords.war set-properties --conf apex_al $OOS_SOURCE_DIR/ords/apex_al.properties
+  java -jar ords.war set-properties --conf apex_rt $OOS_SOURCE_DIR/ords/apex_rt.properties
 else
   echo; echo Manual input required for ORDS config; echo
   echo dbserver: localhost
