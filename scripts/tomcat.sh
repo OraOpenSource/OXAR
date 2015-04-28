7443c8762e5f424c2bc34a64ec0240ec45b53bd9
Trent Schafer <trent.schafer@gmail.com>
2015-04-29 09:12:17 +1000

    Add tomcat port config option

--- scripts/tomcat.sh
+++ scripts/tomcat.sh
@@ -6,6 +6,11 @@ yum install tomcat tomcat-webapps tomcat-admin-webapps -y
 #Add a user into tomcat-users.xml (/etc/tomcat/tomcat-user.xml) as defined in config.properties
 perl -i -p -e "s/<tomcat-users>/<tomcat-users>\n  <\!-- Auto generated content by http\:\/\/www.github.com\/OraOpenSource\/oraclexe-apex install scripts -->\n  <role rolename=\"manager-gui\"\/>\n  <user username=\"${OOS_TC_USERNAME}\" password=\"${OOS_TC_PWD}\" roles=\"manager-gui\"\/>\n  <\!-- End auto-generated content -->/g" /etc/tomcat/tomcat-users.xml
 
+# Set the preferred port
+if [[ "${OOS_ORACLE_TOMCAT_PORT}" != 8080 ]]; then
+  sed -i 's/port\=\"8080\"/port\=\"${OOS_TOMCAT_PORT}\"/' ${CATALINA_HOME}/conf/server.xml
+fi
+
 #Auto start tomcat
 #tomcat service location: /usr/lib/systemd/system/tomcat.service
 systemctl enable tomcat.service
