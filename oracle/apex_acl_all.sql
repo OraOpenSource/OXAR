e5926a738393fcfa9943aaa72071869e70d4f08a
Martin D'Souza <martin.dsouza@gmail.com>
2015-04-29 22:06:10 -0600

    #24: Network ACL all access.

---
+++ oracle/apex_acl_all.sql
@@ -0,0 +1,69 @@
+-- From: https://docs.oracle.com/cd/E59726_01/install.50/e39144/listener.htm#HTMIG29162
+-- With some modifications to handle multiple APEX version numbers
+
+declare
+  acl_path varchar2(4000);
+  l_apex_username all_users.username%type;
+begin
+
+  if upper(user) != 'SYS' then
+    raise_application_error(-20000, 'User must be SYS');
+  end if;
+
+  -- Look for the ACL currently assigned to '*' and give APEX_050000
+  -- the "connect" privilege if APEX_050000 does not have the privilege yet.
+
+  select acl
+  into acl_path
+  from dba_network_acls
+  where host = '*'
+    and lower_port is null
+    and upper_port is null;
+
+  -- To support backwards compatibility with APEX 4.2 and APEX 5.0 need to find max APEX version
+  select username
+  into l_apex_username
+  from (
+    select x.*,
+      row_number() over (order by x.apex_version_num desc) rn
+    from (
+      select
+        username,
+        to_number(regexp_substr(username, '([[:digit:]]+)$')) apex_version_num
+      from all_users
+      where 1=1
+        and regexp_like(username, '^apex_([[:digit:]]+)', 'i')
+      ) x
+    )
+  where rn = 1;
+
+
+  if dbms_network_acl_admin.check_privilege(acl_path, l_apex_username, 'connect') IS NULL THEN
+    dbms_network_acl_admin.add_privilege(
+      acl_path,
+      l_apex_username,
+      true,
+      'connect');
+  end if;
+
+exception
+  -- when no acl has been assigned to '*'.
+  when no_data_found then
+    dbms_network_acl_admin.create_acl(
+      'power_users.xml',
+      'ACL that lets power users to connect to everywhere',
+      l_apex_username,
+      true, 'connect');
+    dbms_network_acl_admin.assign_acl('power_users.xml','*');
+end;
+/
+commit;
+
+-- Can check the network ACLS using the following queries
+--
+-- select host, lower_port, upper_port, acl
+-- from dba_network_acls;
+--
+-- select acl, principal
+-- from dba_network_acl_privileges;
+-- 
