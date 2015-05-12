c93a34ffc1e4bf9f82a913d675ffe86f6f8b9ddc
Trent Schafer <trent.schafer@gmail.com>
2015-05-12 13:55:32 +1000

    Unlock sample data. Close #84

--- scripts/oracle_config.sh
+++ scripts/oracle_config.sh
@@ -24,3 +24,7 @@ echo Setting default tablespace
 sqlplus sys/${OOS_ORACLE_PWD} as sysdba @default_tablespace.sql << EOF1
 USERS
 EOF1
+
+#Unlock sample data as described in docs: http://docs.oracle.com/cd/E17781_01/admin.112/e18585/toc.htm#XEGSG120
+echo Unlocking sample data \(schema: hr\)
+sqlplus sys/${OOS_ORACLE_PWD} as sysdba @unlock_sample_data.sql
