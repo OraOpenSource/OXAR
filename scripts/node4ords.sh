11800fdbb8d54bbcc4ec9d22ae52ddc4c78776c5
Trent Schafer <trent.schafer@gmail.com>
2015-05-25 09:19:56 +1000

    Suppress error out of hash

--- scripts/node4ords.sh
+++ scripts/node4ords.sh
@@ -15,7 +15,7 @@ cd ${OOS_SOURCE_DIR}
 cp node4ords/node4ords.conf /etc/node4ords.conf
 cp node4ords/node4ords /usr/local/bin/node4ords
 
-if hash systemctl; then
+if hash systemctl 2>/dev/null; then
     cp -f init.d/node4ords.service /etc/systemd/system/
     cp node4ords/rsyslog.conf /etc/rsyslog.d/node4ords.conf
     ${OOS_SERVICE_CTL} restart rsyslog
