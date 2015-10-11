9f3e928f171b35c07983e57705d60af5a4e6325c
Martin D'Souza <martin.dsouza@gmail.com>
2015-10-11 09:40:32 -0600

    Minor

--- utils/echo_title.sh
+++ utils/echo_title.sh
@@ -2,7 +2,7 @@
 # echo_title.sh
 # Purpose: Wrapper for echoing titles
 args=$#
-title=$1
+OOS_ECHO_TITLE=$1
 
 if [[ $args -ne 1 ]]; then
   echo "echo_title.sh: Invalid number of arguments" >&2
@@ -10,4 +10,4 @@ if [[ $args -ne 1 ]]; then
   exit 1
 fi
 
-(echo; echo \* $title \*; echo) | tee ${OOS_INSTALL_LOG} --append
+(echo; echo \* $OOS_ECHO_TITLE \*; echo) | tee ${OOS_INSTALL_LOG} --append
