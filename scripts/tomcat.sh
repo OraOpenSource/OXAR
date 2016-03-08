1a799ed02c7a4728aa3b545c8648ee9df6a25733
Trent Schafer <trent.schafer@gmail.com>
2016-03-08 12:08:14 +1100

    Use tomcat config from template This is to make it easier to manage updates to the config file. Whilst using APEX I found changing script source in supporting objects through the editor, the save couldn't be applied. This relates to needing maxPostSize being set to a value larger than 2MB (the default).See #135

--- scripts/tomcat.sh
+++ scripts/tomcat.sh
@@ -26,12 +26,12 @@ ${OOS_SERVICE_CTL} stop ${TOMCAT_SERVICE_NAME}
 #Add a user into tomcat-users.xml (/etc/tomcat/tomcat-user.xml) as defined in config.properties
 perl -i -p -e "s/<tomcat-users>/<tomcat-users>\n  <\!-- Auto generated content by http\:\/\/www.github.com\/OraOpenSource\/oraclexe-apex install scripts -->\n  <role rolename=\"manager-gui\"\/>\n  <user username=\"${OOS_TOMCAT_USERNAME}\" password=\"${OOS_TOMCAT_PASSWORD}\" roles=\"manager-gui\"\/>\n  <\!-- End auto-generated content -->/g" ${CATALINA_HOME}/conf/tomcat-users.xml
 
+# Copy the configuration template over
+cp -f ${CATALINA_HOME}/conf/server.xml ${CATALINA_HOME}/conf/server_original.xml
+cp -f $OOS_SOURCE_DIR/tomcat/server.xml ${CATALINA_HOME}/conf/server.xml
+
 # Set the preferred port
-if [[ "${OOS_TOMCAT_PORT}" != 8080 ]]; then
-
-  sed -i "s/port\=\"8080\"/port\=\"${OOS_TOMCAT_PORT}\"/" ${CATALINA_HOME}/conf/server.xml
-fi
-
+sed -i "s/OOS_TOMCAT_SERVER_PORT/${OOS_TOMCAT_PORT}/" ${CATALINA_HOME}/conf/server.xml
 
 ${OOS_SERVICE_CTL} enable ${TOMCAT_SERVICE_NAME}
 ${OOS_SERVICE_CTL} start ${TOMCAT_SERVICE_NAME}
