77efa5ec4da85d589662fc08bcecd394e0923a19
Martin D'Souza <martin.dsouza@gmail.com>
2016-08-07 11:54:48 -0600

    #170 AOP ACL

---
+++ addons/aop/aop_setup.sql
@@ -0,0 +1,25 @@
+-- Takes in the following parameters:
+-- 1: AOP user name
+
+define aop_user_ame = '&1'
+
+
+declare
+  c_acl constant varchar2(255) := 'aop.xml';
+begin
+
+  if upper(user) != 'SYS' then
+    raise_application_error(-20000, 'User must be SYS');
+  end if;
+
+  dbms_network_acl_admin.create_acl(
+    c_acl,
+    'AOP ACL',
+    &aop_user_ame.,
+    true,
+    'connect');
+  dbms_network_acl_admin.assign_acl(c_acl,'*');
+
+  commit;
+end;
+/
--- scripts/aop.sh
+++ scripts/aop.sh
@@ -17,3 +17,9 @@ echo exit | sqlplus sys/$OOS_ORACLE_PWD as sysdba @apex/create_workspace.sql $OO
 # Create AOP Workspace User
 echo creating AOP APEX User
 echo exit | sqlplus sys/$OOS_ORACLE_PWD as sysdba @apex/create_user.sql $OOS_AOP_APEX_WORKSPACE $OOS_AOP_SCHEMA_NAME $OOS_AOP_APEX_USER_NAME $OOS_AOP_APEX_USER_PWD
+
+
+# AOP ACL
+echo Setting up AOP
+cd $OOS_SOURCE_DIR/addons/aop
+echo exit | sqlplus sys/$OOS_ORACLE_PWD as sysdba @aop_setup.sql $OOS_AOP_SCHEMA_NAME
