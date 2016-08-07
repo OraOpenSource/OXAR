6c82c2435346e455f6efa5bcee51f395010e48ab
Martin D'Souza <martin.dsouza@gmail.com>
2016-08-07 13:05:56 -0600

    #170 AOP ACL

--- addons/aop/aop_setup.sql
+++ addons/aop/aop_setup.sql
@@ -15,7 +15,7 @@ begin
   dbms_network_acl_admin.create_acl(
     c_acl,
     'AOP ACL',
-    &aop_user_ame.,
+    '&aop_user_ame.',
     true,
     'connect');
   dbms_network_acl_admin.assign_acl(c_acl,'*');
