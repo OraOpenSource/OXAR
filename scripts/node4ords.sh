590e54de2103e7817f2a7c5213e9105b73c78a40
Martin D'Souza <martin.dsouza@gmail.com>
2016-09-04 06:48:37 -0600

    #176 start node4ords via pm2 rather than from systemctl

--- scripts/node4ords.sh
+++ scripts/node4ords.sh
@@ -10,13 +10,19 @@ sed -i "s/http\:\/\/localhost:8080/http\:\/\/localhost:${OOS_TOMCAT_PORT}/" conf
 npm install --unsafe-perm
 
 #Start on boot
-cd ${OOS_SOURCE_DIR}
+# 176 Use pm2 instead of systemctl
+pm2 start app.js --name="node4ords" --watch
+pm2 save
 
-cp node4ords/node4ords.conf /etc/node4ords.conf
-cp node4ords/node4ords /usr/local/bin/node4ords
-
-cp -f init.d/node4ords.service /etc/systemd/system/
-cp node4ords/rsyslog.conf /etc/rsyslog.d/node4ords.conf
-systemctl restart rsyslog
-systemctl enable node4ords.service
-systemctl start node4ords.service
+# TODO mdsouza: if this works delete everything below along with corresponding files
+# TODO mdsouza: update documentation (remove old, document pm2)
+# cd ${OOS_SOURCE_DIR}
+#
+# cp node4ords/node4ords.conf /etc/node4ords.conf
+# cp node4ords/node4ords /usr/local/bin/node4ords
+#
+# cp -f init.d/node4ords.service /etc/systemd/system/
+# cp node4ords/rsyslog.conf /etc/rsyslog.d/node4ords.conf
+# systemctl restart rsyslog
+# systemctl enable node4ords.service
+# systemctl start node4ords.service
