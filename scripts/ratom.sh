49ea56587d431287a19a862ade9bee7d3e1a7708
Martin D'Souza <martin@clarifit.com>
2018-11-20 23:13:45 -0700

    cleanup files from 1.0.0 to focus on docker build

--- scripts/ratom.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#*** REMOTE ATOM ***
-#https://github.com/randy3k/remote-atom
-#This is to make files easier to edit in Atom
-cd $OOS_SOURCE_DIR/tmp
-${OOS_UTILS_DIR}/download.sh https://raw.githubusercontent.com/aurora/rmate/master/rmate
-chmod +x rmate
-mv rmate /usr/local/bin/ratom
